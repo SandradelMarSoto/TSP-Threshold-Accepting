497ae7d32d270ead00aa2dd515a2712b24459e16
Angel Gladin <angelgladin@gmail.com>
2019-10-06 02:11:56 -0500

    Final tuning for the system :squirrel:

--- src/main/kotlin/unam/ciencias/heuristicas/Constants.kt
+++ src/main/kotlin/unam/ciencias/heuristicas/Constants.kt
@@ -2,30 +2,30 @@ package unam.ciencias.heuristicas
 
 /**
  * Variables for tuning our TSP system.
- *
  */
 class Constants {
     companion object {
+        /** Path of the database used */
+        const val DB_URL = "jdbc:sqlite:db/tsp.db"
 
-        /** TODO */
-        const val EPSILON = 0.01
-        /** TODO */
-        const val EPSILON_P = 0.01
+        /** Epsilon used in Threshold Accepting heuristic */
+        const val EPSILON = 0.0001
+        /** Epsilon used in the initial temperature algorithm */
+        const val EPSILON_P = 0.0001
         /** Initial system temperature */
         const val T = 8.0
         /** Upper bound for iterations when calculating a batch */
-        const val L: Int = 10e3.toInt()
+        const val L = 2000
         /** Cooling factor that determines how slow or fast the temperature [T] it's going to decrease */
-        const val PHI = 0.5
+        const val PHI = 0.9
         /** Earth radius in meters */
         const val EARTH_RADIUS_IN_METERS = 6373000.0
-        /** Probability */
-        const val P = 0.85
-        /** Number of cities 1092 */
-        const val NUM_CITIES = 1092
-        /** TODO */
-        const val ACCEPTED_NEIGHBORS = 40
-
+        /** Percentage */
+        const val P = 0.6
+        /**  Value of accepted neighbors used when calculating the initial temperature. */
+        const val ACCEPTED_NEIGHBORS = 100
+        /** Maximum number of attempts when calculating a batch. */
+        const val MAX_ITERATIONS = L * 42
 
     }
 }
